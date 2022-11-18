1e714741738eabac02f73705d7d807cdd13d6c3b
l02 <l8556@outlook.com>
2022-11-18 17:43:10 +0300

    Added xml generation to run x2ttester
    fixup! Added xml generation to run x2ttester
    
    fixup! Added xml generation to run x2ttester

--- framework/actions/xml_actions.rb
+++ framework/actions/xml_actions.rb
@@ -21,4 +21,42 @@ class XmlActions
   def self.create_doc_renderer_config(path_to)
     File.write("#{path_to}/DoctRenderer.config", generate_doc_renderer_xml(StaticData::X2T_CONNECTIONS))
   end
+
+  # @param [String] input_dir Path to directory with source files for conversion
+  # @param [String] output_dir Path to the directory where the converted file will be saved
+  # @param [String] cores Number of threads to convert
+  # @param [String (frozen)] direction Conversion direction
+  # @return [Tempfile]
+  def self.generate_param_for_x2ttester(cores, direction = 'all')
+    xml_parameters = Nokogiri::XML::Builder.new(encoding: 'UTF-8') do |xml|
+      xml.Settings do
+        xml.reportPath("#{StaticData::REPORTS_DIR}/#{direction}_report.csv")
+        xml.inputDirectory(StaticData::CONFIG.fetch('input_dir'))
+        xml.outputDirectory(StaticData::CONFIG.fetch('output_dir'))
+        xml.x2tPath(StaticData::X2T_PATH)
+        xml.cores(cores)
+        xml.input(direction) if direction != 'all'
+        if Dir.exist?(StaticData::FONTS_DIR) && !Dir.empty?(StaticData::FONTS_DIR)
+          xml.fonts('system' => '0') do
+            xml.directory(StaticData::FONTS_DIR)
+          end
+        else
+          xml.fonts('system' => '1') do
+            xml.directory('')
+          end
+        end
+      end
+    end
+    XmlActions.write_xml_to_tmp_file(xml_parameters)
+  end
+
+  # Creates a unique temporary xml-file
+  # @param [Object] xml_parameters
+  # @return [null, Tempfile]
+  def self.write_xml_to_tmp_file(xml_parameters)
+    file = Tempfile.new(%w[params .xml], StaticData::TMP_DIR)
+    file.write(xml_parameters.to_xml)
+    file.rewind
+    file
+  end
 end
