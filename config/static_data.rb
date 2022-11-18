8a8fda51fe512bcd3ee01f7be919b8af415c7beb
l02 <l8556@outlook.com>
2022-11-18 19:29:32 +0300

    minor refactoring
    fixup! minor refactoring

--- config/static_data.rb
+++ config/static_data.rb
@@ -1,14 +1,12 @@
 # frozen_string_literal: true
 
-require_relative '../manager'
-
 # class with some constants and static data
 class StaticData
   PROJECT_DIR = Dir.pwd
   TMP_DIR = "#{PROJECT_DIR}/tmp/"
   CORE_DIR = "#{PROJECT_DIR}/core/"
   FONTS_DIR = "#{PROJECT_DIR}/assets/fonts/"
-  HOST, ARCH, X2T, X2TTESTER, STANDARDTESTER = HostConfig.host_information
+  HOST, ARCH, X2T, X2TTESTER, STANDARDTESTER = HostActions.host_validations
   X2T_PATH = "#{CORE_DIR}/#{X2T}"
   REPORTS_DIR = "#{PROJECT_DIR}/reports/"
 
