a16570a71b730747f05a9d0cd09abdff72d01978
l02 <l8556@outlook.com>
2022-11-21 15:48:26 +0300

    Fixed x2t startup for macOS

--- framework/actions/host_actions.rb
+++ framework/actions/host_actions.rb
@@ -1,14 +1,22 @@
 # frozen_string_literal: true
 
 # Getting information's about host
-class HostConfig
+class HostActions
+
+  # Settings for running x2t on macOS
+  # DYLD_LIBRARY_PATH environment variable is needed to start x2t correctly from any folder
+  def self.mac_configuration
+    ENV['DYLD_LIBRARY_PATH'] = StaticData::CORE_DIR.to_s
+  end
+
   # Returns the names of x2t utilities and information about the OS
   # @return [[String, String, String, String, String]] os, arch, x2t, x2ttester, standardtester
-  def self.host_information
+  def self.host_validations
     case Gem::Platform.local.os
     when 'linux'
       ['linux', Gem::Platform.local.cpu.sub('86_', ''), 'x2t', 'x2ttester', 'standardtester']
     when 'darwin'
+      HostActions.mac_configuration
       ['mac', Gem::Platform.local.cpu.sub('arm64', 'arm'), 'x2t', 'x2ttester', 'standardtester']
     when 'mingw'
       ['windows', Gem::Platform.local.cpu, 'x2t.exe', 'x2ttester.exe', 'standardtester.exe']
