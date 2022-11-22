741a9549f02ab2f842bb5613a6a8081c7403440a
l02 <l8556@outlook.com>
2022-11-22 16:28:59 +0300

    Added a wrapper for the new x2ttester functionality - conversion according to the specified direction - conversion from a list of filenames - delete successful conversions files - timestamp in report file name - reports only errors

--- framework/actions/xml_actions.rb
+++ framework/actions/xml_actions.rb
@@ -5,7 +5,26 @@ class XmlActions
   def initialize
     @x2t_config = JSON.load_file("#{Dir.pwd}/config.json")
     @x2t_connections = JSON.load_file("#{Dir.pwd}/config/x2t_connections.json")
-    @x2tpath = "#{ProjectConfig.core_dir}/#{ProjectConfig.host_config[:x2t]}"
+  end
+
+  def generate_x2t_path
+    raise('Check x2t File') unless File.exist?("#{ProjectConfig.core_dir}/#{ProjectConfig.host_config[:x2t]}")
+
+    "#{ProjectConfig.core_dir}/#{ProjectConfig.host_config[:x2t]}"
+  end
+
+  def generate_number_of_cores
+    raise('Please enter the number of cores in config.json ') if @x2t_config.fetch('cores') == ''
+
+    @x2t_config.fetch('cores')
+  end
+
+  def generate_error_only
+    raise('Check errors only parameter in config.json') unless (@x2t_config.fetch('errors_only') != '') ||
+                                                               (@x2t_config.fetch('errors_only') != '0') ||
+                                                               (@x2t_config.fetch('errors_only') != '1')
+
+    @x2t_config.fetch('errors_only')
   end
 
   def generate_doc_renderer_xml
@@ -24,19 +43,34 @@ class XmlActions
     File.write("#{ProjectConfig.core_dir}/DoctRenderer.config", generate_doc_renderer_xml)
   end
 
+  def generate_files_lis
+    xml_parameters = Nokogiri::XML::Builder.new(encoding: 'UTF-8') do |xml|
+      xml.files do
+        @x2t_config.fetch('files_array').each do |file_name|
+          xml.file(file_name)
+        end
+      end
+    end
+    write_xml_to_tmp_file(xml_parameters)
+  end
+
   # Generate parameters for x2ttester
-  # @param [String] cores Number of threads to convert
   # @param [String (frozen)] direction Conversion direction
   # @return [Tempfile]
-  def generate_parameters(cores, direction = 'all')
+  def generate_parameters(input_format = nil, output_format = nil, path_to_files_list = nil)
     xml_parameters = Nokogiri::XML::Builder.new(encoding: 'UTF-8') do |xml|
       xml.Settings do
-        xml.reportPath("#{ProjectConfig.reports_dir}/#{direction}_report.csv")
+        xml.reportPath("#{ProjectConfig.reports_dir}/#{input_format}_#{output_format}_report.csv")
         xml.inputDirectory(@x2t_config.fetch('input_dir'))
         xml.outputDirectory(@x2t_config.fetch('output_dir'))
-        xml.x2tPath(@x2tpath)
-        xml.cores(cores)
-        xml.input(direction) if direction != 'all'
+        xml.x2tPath(generate_x2t_path)
+        xml.cores(generate_number_of_cores)
+        xml.input(input_format) if input_format
+        xml.output(output_format) if output_format
+        xml.errorsOnly(generate_error_only) if @x2t_config.fetch('errors_only') != ''
+        xml.deleteOk(@x2t_config.fetch('delete')) if @x2t_config.fetch('delete') != ''
+        xml.timestamp(@x2t_config.fetch('timestamp')) if @x2t_config.fetch('timestamp') != ''
+        xml.inputFilesList(path_to_files_list) if path_to_files_list
         if Dir.exist?(ProjectConfig.fonts_dir) && !Dir.empty?(ProjectConfig.fonts_dir)
           xml.fonts('system' => '0') do
             xml.directory(ProjectConfig.fonts_dir)
@@ -61,3 +95,4 @@ class XmlActions
     file
   end
 end
+
